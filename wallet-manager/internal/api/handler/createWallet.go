2873b74135dd1804185087d8280eb7b4bf8bbb1f
Mahmoud Ahmadi <mahmoud.ahmadi@arvancloud.ir>
2026-02-25 13:21:42 +0330

    feat: initiated the api handlers

---
+++ wallet-manager/internal/api/handler/createWallet.go
@@ -0,0 +1,9 @@
+package handler
+
+import (
+	"github.com/gofiber/fiber/v3"
+)
+
+func CreateWallet(c *fiber.Ctx) error {
+	return nil
+}
---
+++ wallet-manager/internal/api/handler/deleteWallet.go
@@ -0,0 +1,7 @@
+package handler
+
+import "github.com/gofiber/fiber/v3"
+
+func DeleteWallet(c *fiber.Ctx) error {
+	return nil
+}
