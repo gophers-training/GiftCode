3c902aa210aaa50d563415444d108bc2572d3fea
Mahmoud Ahmadi <mahmoud.ahmadi@arvancloud.ir>
2026-02-25 13:28:49 +0330

    feat: added cors config to the api server

--- wallet-manager/internal/api/server.go
+++ wallet-manager/internal/api/server.go
@@ -1,10 +1,12 @@
 package apisvc
 
 import (
+	"strings"
 	"sync"
 	"wallet-manager/config"
 
 	"github.com/gofiber/fiber/v3"
+	"github.com/gofiber/fiber/v3/middleware/cors"
 )
 
 func StartAPIServer(wg *sync.WaitGroup) {
@@ -12,8 +14,20 @@ func StartAPIServer(wg *sync.WaitGroup) {
 
 	api := fiber.New()
 
+	// Set CORS configuration
+	SetCorsConfig(api)
+
 	err := api.Listen(config.AppConfig.API.GetListenAddress())
 	if err != nil {
 		panic("Failed to start API server: " + err.Error())
 	}
+}
+
+func SetCorsConfig(api *fiber.App) {
+	api.Use(cors.New(cors.Config{
+		AllowOrigins:     strings.Split(config.AppConfig.API.CORS.AllowedOrigins, ","),
+		AllowMethods:     strings.Split(config.AppConfig.API.CORS.AllowedMethods, ","),
+		AllowHeaders:     strings.Split(config.AppConfig.API.CORS.AllowedHeaders, ","),
+		AllowCredentials: config.AppConfig.API.CORS.AllowCredentials,
+	}))
 }
