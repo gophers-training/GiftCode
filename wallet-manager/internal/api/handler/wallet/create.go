b036c994f3ed0ae183890fee33a9ee719a36616f
Mahmoud Ahmadi <mahmoud.ahmadi@arvancloud.ir>
2026-02-25 16:02:58 +0330

    feat: added wallet create api

--- wallet-manager/internal/api/handler/wallet/create.go
+++ wallet-manager/internal/api/handler/wallet/create.go
@@ -1,9 +1,36 @@
 package wallethandler
 
 import (
+	"wallet-manager/internal/model"
+	walletsvc "wallet-manager/internal/service/wallet"
+
 	"github.com/gofiber/fiber/v3"
 )
 
 func Create(c fiber.Ctx) error {
+	createRequest := model.CreateWalletRequest{}
+	if err := c.Bind().JSON(&createRequest); err != nil {
+		errorMessage := model.FailureResponse{
+			Message: "Invalid request payload",
+			Error:   err.Error(),
+		}
+		return c.Status(fiber.StatusBadRequest).JSON(errorMessage)
+	}
+
+	createWallet, err := walletsvc.Create(createRequest)
+	if err != nil {
+		errorMessage := model.FailureResponse{
+			Message: "Failed to create wallet",
+			Error:   err.Error(),
+		}
+		return c.Status(fiber.StatusInternalServerError).JSON(errorMessage)
+	}
+
+	response := model.ResponseWithData{
+		Message: "Wallet created successfully",
+		Data:    createWallet,
+	}
+	c.Status(fiber.StatusCreated).JSON(response)
+
 	return nil
 }
--- wallet-manager/internal/service/wallet/create.go
+++ wallet-manager/internal/service/wallet/create.go
@@ -5,6 +5,19 @@ import (
 	inmemory "wallet-manager/internal/repository/memory"
 )
 
-func Create(w model.Wallet) error {
-	return inmemory.CreateWallet(w)
+func Create(w model.CreateWalletRequest) (model.Wallet, error) {
+	newWallet := model.Wallet{
+		Mobile: w.Mobile,
+	}
+	err := inmemory.CreateWallet(newWallet)
+	if err != nil {
+		return model.Wallet{}, err
+	}
+
+	createdWallet, err := inmemory.GetWalletDetails(w.Mobile)
+	if err != nil {
+		return model.Wallet{}, err
+	}
+
+	return createdWallet, nil
 }
