698470c5ec3db4fea51618c7dd3d5a122e468cb5
Mahmoud Ahmadi <mahmoud.ahmadi@arvancloud.ir>
2026-02-25 15:42:28 +0330

    feat: added Get wallet list api

--- wallet-manager/internal/api/handler/wallet/list.go
+++ wallet-manager/internal/api/handler/wallet/list.go
@@ -1,7 +1,33 @@
 package wallethandler
 
-import "github.com/gofiber/fiber/v3"
+import (
+	"wallet-manager/internal/model"
+	walletsvc "wallet-manager/internal/service/wallet"
+
+	"github.com/gofiber/fiber/v3"
+)
 
 func List(c fiber.Ctx) error {
-	return nil
+	walletList, err := walletsvc.List()
+	if err != nil {
+		errorMessage := model.FailureResponse{
+			Message: "Failed to retrieve wallet list",
+			Error:   err.Error(),
+		}
+		return c.Status(fiber.StatusInternalServerError).JSON(errorMessage)
+	}
+
+	if len(walletList) == 0 {
+		response := model.ResponseWithData{
+			Message: "No wallets found",
+			Data:    []model.Wallet{},
+		}
+		return c.Status(fiber.StatusOK).JSON(response)
+	}
+
+	response := model.ResponseWithData{
+		Message: "Wallet list retrieved successfully",
+		Data:    walletList,
+	}
+	return c.Status(fiber.StatusOK).JSON(response)
 }
